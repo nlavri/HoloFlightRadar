00db5cd9dab1cb0f31e0a00fdf50289db3750830
nlavri <nlavri@gmail.com>
2017-07-03 22:37:31 +0300

    gazeAndCursor

--- Assets/GazeManager.cs
+++ Assets/GazeManager.cs
@@ -1,115 +1,116 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using HoloToolkit.Unity;
+﻿using HoloToolkit.Unity;
 using HoloToolkit.Unity.InputModule;
 using UnityEngine;
 
-/// <summary>
-/// GazeManager determines the location of the user's gaze, hit position and normals.
-/// </summary>
-public class GazeManager : Singleton<GazeManager>
+namespace Assets
 {
-    [Tooltip("Maximum gaze distance for calculating a hit.")]
-    public float MaxGazeDistance = 5.0f;
-
-    [Tooltip("Select the layers raycast should target.")]
-    public LayerMask RaycastLayerMask = Physics.DefaultRaycastLayers;
-
     /// <summary>
-    /// Physics.Raycast result is true if it hits a Hologram.
+    /// GazeManager determines the location of the user's gaze, hit position and normals.
     /// </summary>
-    public bool Hit { get; private set; }
-
-    /// <summary>
-    /// HitInfo property gives access
-    /// to RaycastHit public members.
-    /// </summary>
-    public RaycastHit HitInfo { get; private set; }
-
-    /// <summary>
-    /// Position of the user's gaze.
-    /// </summary>
-    public Vector3 Position { get; private set; }
-
-    /// <summary>
-    /// RaycastHit Normal direction.
-    /// </summary>
-    public Vector3 Normal { get; private set; }
-
-    private GazeStabilizer gazeStabilizer;
-    private Vector3 gazeOrigin;
-    private Vector3 gazeDirection;
-
-    void Awake()
+    public class GazeManager : Singleton<GazeManager>
     {
-        /* TODO: DEVELOPER CODING EXERCISE 3.a */
+        [Tooltip("Maximum gaze distance for calculating a hit.")]
+        public float MaxGazeDistance = 5.0f;
 
-        // 3.a: GetComponent GazeStabilizer and assign it to gazeStabilizer.
+        [Tooltip("Select the layers raycast should target.")]
+        public LayerMask RaycastLayerMask = Physics.DefaultRaycastLayers;
 
-    }
+        /// <summary>
+        /// Physics.Raycast result is true if it hits a Hologram.
+        /// </summary>
+        public bool Hit { get; private set; }
 
-    private void Update()
-    {
-        // 2.a: Assign Camera's main transform position to gazeOrigin.
-        gazeOrigin = Camera.main.transform.position;
+        /// <summary>
+        /// HitInfo property gives access
+        /// to RaycastHit public members.
+        /// </summary>
+        public RaycastHit HitInfo { get; private set; }
 
-        // 2.a: Assign Camera's main transform forward to gazeDirection.
-        gazeDirection = Camera.main.transform.forward;
+        /// <summary>
+        /// Position of the user's gaze.
+        /// </summary>
+        public Vector3 Position { get; private set; }
 
-        // 3.a: Using gazeStabilizer, call function UpdateHeadStability.
-        // Pass in gazeOrigin and Camera's main transform rotation.
+        /// <summary>
+        /// RaycastHit Normal direction.
+        /// </summary>
+        public Vector3 Normal { get; private set; }
 
+        private GazeStabilizer gazeStabilizer;
+        private Vector3 gazeOrigin;
+        private Vector3 gazeDirection;
 
-        // 3.a: Using gazeStabilizer, get the StableHeadPosition and
-        // assign it to gazeOrigin.
-
-
-        UpdateRaycast();
-    }
-
-    /// <summary>
-    /// Calculates the Raycast hit position and normal.
-    /// </summary>
-    private void UpdateRaycast()
-    {
-        /* TODO: DEVELOPER CODING EXERCISE 2.a */
-
-        // 2.a: Create a variable hitInfo of type RaycastHit.
-        RaycastHit hitInfo;
-
-        // 2.a: Perform a Unity Physics Raycast.
-        // Collect return value in public property Hit.
-        // Pass in origin as gazeOrigin and direction as gazeDirection.
-        // Collect the information in hitInfo.
-        // Pass in MaxGazeDistance and RaycastLayerMask.
-        Hit = Physics.Raycast(gazeOrigin,
-                       gazeDirection,
-                       out hitInfo,
-                       MaxGazeDistance,
-                       RaycastLayerMask);
-
-        // 2.a: Assign hitInfo variable to the HitInfo public property 
-        // so other classes can access it.
-        HitInfo = hitInfo;
-
-        if (Hit)
+        void Awake()
         {
-            // If raycast hit a hologram...
+            /* TODO: DEVELOPER CODING EXERCISE 3.a */
 
-            // 2.a: Assign property Position to be the hitInfo point.
-            Position = hitInfo.point;
-            // 2.a: Assign property Normal to be the hitInfo normal.
-            Normal = hitInfo.normal;
+            // 3.a: GetComponent GazeStabilizer and assign it to gazeStabilizer.
+
         }
-        else
-        {
-            // If raycast did not hit a hologram...
-            // Save defaults ...
 
-            // 2.a: Assign Position to be gazeOrigin plus MaxGazeDistance times gazeDirection.
-            Position = gazeOrigin + (gazeDirection * MaxGazeDistance);
-            // 2.a: Assign Normal to be the user's gazeDirection.
-            Normal = gazeDirection;
+        private void Update()
+        {
+            // 2.a: Assign Camera's main transform position to gazeOrigin.
+            gazeOrigin = Camera.main.transform.position;
+
+            // 2.a: Assign Camera's main transform forward to gazeDirection.
+            gazeDirection = Camera.main.transform.forward;
+
+            // 3.a: Using gazeStabilizer, call function UpdateHeadStability.
+            // Pass in gazeOrigin and Camera's main transform rotation.
+
+
+            // 3.a: Using gazeStabilizer, get the StableHeadPosition and
+            // assign it to gazeOrigin.
+
+
+            UpdateRaycast();
+        }
+
+        /// <summary>
+        /// Calculates the Raycast hit position and normal.
+        /// </summary>
+        private void UpdateRaycast()
+        {
+            /* TODO: DEVELOPER CODING EXERCISE 2.a */
+
+            // 2.a: Create a variable hitInfo of type RaycastHit.
+            RaycastHit hitInfo;
+
+            // 2.a: Perform a Unity Physics Raycast.
+            // Collect return value in public property Hit.
+            // Pass in origin as gazeOrigin and direction as gazeDirection.
+            // Collect the information in hitInfo.
+            // Pass in MaxGazeDistance and RaycastLayerMask.
+            Hit = Physics.Raycast(gazeOrigin,
+                gazeDirection,
+                out hitInfo,
+                MaxGazeDistance,
+                RaycastLayerMask);
+
+            // 2.a: Assign hitInfo variable to the HitInfo public property 
+            // so other classes can access it.
+            HitInfo = hitInfo;
+
+            if (Hit)
+            {
+                // If raycast hit a hologram...
+
+                // 2.a: Assign property Position to be the hitInfo point.
+                Position = hitInfo.point;
+                // 2.a: Assign property Normal to be the hitInfo normal.
+                Normal = hitInfo.normal;
+            }
+            else
+            {
+                // If raycast did not hit a hologram...
+                // Save defaults ...
+
+                // 2.a: Assign Position to be gazeOrigin plus MaxGazeDistance times gazeDirection.
+                Position = gazeOrigin + (gazeDirection * MaxGazeDistance);
+                // 2.a: Assign Normal to be the user's gazeDirection.
+                Normal = gazeDirection;
+            }
         }
     }
 }
